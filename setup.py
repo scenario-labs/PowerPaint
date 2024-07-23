f5dc7650519fd98475a7cb1663d1dda2336ddb98
Clem <70368164+Clement-Lelievre@users.noreply.github.com>
2024-07-23 17:02:14 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+REQUIREMENTS = [i.strip() for i in open("requirements/requirements.txt").readlines()]
+
+
+setup(
+    name="powerpaint",
+    version="0.1.1",
+    install_requires=REQUIREMENTS,
+    packages=["powerpaint"],
+    url="https://github.com/scenario-labs/PowerPaint"
+)
